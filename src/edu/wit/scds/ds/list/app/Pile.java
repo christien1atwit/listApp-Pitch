e67c6d846737aa7515a57406fbd806709863e0d4
yeek1 <yeek1@PF2XVABV.wit.private>
2022-11-28 15:36:00 -0500

    Hello.

--- src/edu/wit/scds/ds/list/app/Pile.java
+++ src/edu/wit/scds/ds/list/app/Pile.java
@@ -24,7 +24,7 @@
 
 package edu.wit.scds.ds.list.app ;
 
-import java.util.ArrayList ;
+import java.util.LinkedList ;
 import java.util.List ;
 
 /**
@@ -32,32 +32,68 @@ import java.util.List ;
  * <p>
  * the bottom card is at position 0
  *
- * @author Nathan Christie
+ * @author Your Name
  *
  * @version 1.0.0 2022-11-15 Initial implementation
+ * @version 1.0.1 2022-11-19 Template implementation
+ * @version 1.0.2 2022-11-20 Template implementation 2
  */
 public class Pile
     {
-
-    // data fields
-    /** the list of cards - directly accessible by subclasses */
-    protected List<Card> cards ;    // instantiate an ArrayList or LinkedList in the constructor
-
-
-    // TODO implement this
-    /**
-     * 
-     */
-    public Pile() 
-        {
-            this.cards=new ArrayList<>();
-        }
     
-    public add(Card cardToAdd)
+    // TODO implement this
+    
+    // Data fields
+    /** the list of cards - directly accessible by subclasses */
+    protected List<Card> cards ;   // instantiate an ArrayList or LinkedList in the constructor
+    protected int numberOfCards ;
+    
+    // Constructor
+    
+    public Pile()
         {
-            this.cards.add( cardToAdd );
-        }   
-
+        // instantiate an ArrayList or LinkedList in the constructor and numberOfCards
+        // TODO implement this
+        this.cards = new LinkedList<>() ;
+        this.numberOfCards = 0 ;
+        
+        }   // end no-arg constructor
+    
+    // API methods
+    
+    public void add( Card newCard )
+        {
+        // increment number of cards as well
+        // TODO implement this
+        this.cards.add( newCard ) ;
+        this.numberOfCards++ ;
+        
+        }   // end add()
+    
+    public Card[] getCards()
+        {
+        // TODO implement this
+        
+        }   // end getCards()
+    
+    public int getNumberOfCards()
+        {
+        // TODO implement this
+        
+        }   // end getNumberOfCards()
+    
+    public boolean isEmpty()
+        {
+        // TODO implement this
+        
+        }   // end isEmpty()
+    
+    public Card remove( Card aCard )
+        {
+        // decrement number of cards as well
+        // TODO implement this
+        
+        }   // end remove()
 
     /*
      * (non-Javadoc)
