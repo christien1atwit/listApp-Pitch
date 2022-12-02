4dd549685e02018367d19d2bc24539dba9c54858
fieldd <fieldd@PF2Z32FP.wit.private>
2022-12-01 22:39:22 -0500

    Merge branch 'main' of git@github.com:christien1atwit/listApp-Pitch.git into main

--- src/edu/wit/scds/ds/list/app/Pitch.java
+++ src/edu/wit/scds/ds/list/app/Pitch.java
@@ -6,7 +6,7 @@ import java.util.Scanner ;
 
 /**
  * 
- * @author Your Name // TODO
+ * @author Dennis Field // TODO
  *
  * @version 1.0.0 2022-11-19 Initial implementation
  * @version 1.0.1 2022-11-19 Template implementation
@@ -18,11 +18,14 @@ public class Pitch
     // TODO implement this
     
     // Data fields
-    private Player currentPlayer ;
-    private RoundPile currentRoundPile ;
+    private static Player currentPlayer ;
+    private static Team currentTeam ;
+    private static RoundPile currentRoundPile ;
     // I have no idea what gameScore is, but if you need it, here it is.
     private int gameScore ;
     private Team[] teams ;
+    private static int bet = 1;
+    private static Player betHolder ;
     
     // main
     
@@ -74,9 +77,15 @@ public class Pitch
          */
         // TODO implement this
         
-        Scanner playerInput = new Scanner(System.in) ;
+        // Initialize Scanner
+        Scanner pInput = new Scanner(System.in) ;
         
-        //intialize two teams
+        // Initialize two teams
+        Team team1 = new Team( new Player( new Hand) , new Player( new Hand ) ) ;
+        Team team2 = new Team( new Player( new Hand) , new Player( new Hand ) ) ;
+        
+        // Initialize currentRoundPile
+        currentRoundPile = new RoundPile() ;
         
         System.out.printf("%nWelcome to Pitch!%nPlease input player one's name: ") ;
         // team1.getPlayers()[0].setName( input.Next() ) ;
@@ -87,9 +96,146 @@ public class Pitch
         System.out.printf("%nPlease input player four's name: ") ;
         // team2.getPlayers()[1].setName( input.Next() ) ;
         
-        for(
+        currentPlayer = team1.getPlayers()[0] ;
+        currentTeam = team1 ;
+        
+        startBet( team1, team2, pInput ) ;
+        
+        startRound( team1, team2, pInput ) ;
+        
+        
         
         }   // end main()
+    
+        private static void startSet()
+            {
+            
+            
+        
+            }
+        
+        private static void startBet( Team team1, Team team2, Scanner pInput )
+            {
+            for( int i = 0; i < 4; i++ )
+                {
+                if( bet == 1 )
+                    {
+                    System.out.printf( "&nNo bets placed. Bet set to 2. Bet holder: %s", currentPlayer.getName() ) ;
+                    break ;
+                    
+                    }
+                takeBet( currentPlayer, pInput) ;
+                nextPlayer( team1, team2) ;
+                
+                }
+            currentPlayer = betHolder ;
+        
+            }
+        
+        private static void startRound( Team team1, Team team2, Scanner pInput )
+            {
+            for( int i = 0; i < 4; i++ )
+                {
+                System.out.printf( "Round Pile: %s" + "Hand: %s" +
+                                   "%n%s, its your turn. What card would you like to play? (# from left in hand) ", 
+                                   currentRoundPile.toString(), currentPlayer.getHand().toString(), currentPlayer.getName() ) ;
+                Boolean gotCard = false ;
+                do
+                    {
+                    // Player input an integer
+                    if( pInput.hasNextInt() )
+                        {
+                        // input integer is within bounds of hand size
+                        if( pInput.nextInt() > 0 && pInput.nextInt() < currentPlayer.getHand().numberOfCards + 1 )
+                            {
+                            Boolean cardPlayed = currentPlayer.getHand().playCard( pInput.nextInt() - 1, currentRoundPile ) ;
+                            // makes sure card was played
+                            if ( cardPlayed )
+                                {
+                                gotCard = true ;
+                                System.out.printf( "%nCard played" ) ;
+                                break ;
+                                }
+                            System.out.printf( "%nCannot play this card." ) ;
+                            
+                            }
+                        
+                        }
+                    // Player didn't input integer
+                    else
+                        {
+                        System.out.printf("Please input a card number between 1 and the number of cards in your hand." ) ;
+                        
+                        }
+                    
+                    } while( !gotCard ) ;
+                
+                }
+        
+            }   
+        
+        private static void takeBet( Player player, Scanner pInput)
+            {
+            System.out.printf( "%nHand: " + player.getHand().toString() + "%n%s, What is your bet? (Current Bet: %f) ", player.getName(), bet ) ;
+            Boolean betTaken = false ;
+            do
+                {
+                if( pInput.hasNextInt() )
+                    {
+                    if ( pInput.nextInt() > bet && pInput.nextInt() < 6 )
+                        {
+                        bet = pInput.nextInt() ;
+                        betHolder = currentPlayer ;
+                        betTaken = true ;
+                        }
+                    }
+                else if ( pInput.next().contentEquals( "Skip" ) )
+                    {
+                    System.out.printf( "%nBet skipped." ) ;
+                    betTaken = true ;
+                    
+                    }
+                else
+                    {
+                    System.out.printf( "%nInvalid input. Please enter a number 2 - 5" ) ;
+                    
+                    }
+                
+                } while( !betTaken ) ;
+            
+            }
+        
+        private static void nextPlayer( Team team1, Team team2 )
+            {
+            if( currentPlayer == team1.getPlayers()[0] )
+                {
+                currentPlayer = team2.getPlayers()[0] ;
+                currentTeam = team2 ;
+                
+                }
+            else if( currentPlayer == team2.getPlayers()[0] )
+                {
+                currentPlayer = team1.getPlayers()[1] ;
+                currentTeam = team1 ;
+                                                
+                }
+            else if( currentPlayer == team1.getPlayers()[1] )
+                {
+                currentPlayer = team2.getPlayers()[1] ;
+                currentTeam = team2 ;
+                
+                }
+            else if( currentPlayer == team2.getPlayers()[1] )
+                {
+                currentPlayer = team1.getPlayers()[0] ;
+                currentTeam = team1 ;
+                
+                }
+            
+            
+            }
+        
+        
 
     }
    // end class Pitch
