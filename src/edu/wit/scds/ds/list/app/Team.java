29334753bae6d7a7123bfc31570f44ee63a5ce90
yeek1 <yeek1@PF2XVABV.wit.private>
2022-12-02 01:27:10 -0500

    fixes.

--- src/edu/wit/scds/ds/list/app/Team.java
+++ src/edu/wit/scds/ds/list/app/Team.java
@@ -18,7 +18,12 @@ public class Team
     // Data fields
     private Player[] players ;
     private List<RoundPile> roundPiles ;
+    private int tallyPoints ;
     private int score ;
+    private Card highestCard ;
+    private Card lowestCard ;
+    private RoundPile highestRoundPile ;
+    private RoundPile lowestRoundPile ;
 
     /**
      * @param player1
@@ -33,6 +38,7 @@ public class Team
         // DONE implement this
         this.players[ 0 ] = player1 ;
         this.players[ 1 ] = player2 ;
+        this.tallyPoints = 0 ;
         this.score = 0 ;
 
         }   // end 2-args constructor
@@ -57,8 +63,36 @@ public class Team
         // Does not need number of face cards, since the points of associated ranks
         // should
         // be tallied in aRoundPile
-        addScore( aRoundPile.getTallyPoints() ) ;
+
+        this.tallyPoints += aRoundPile.getTallyPoints() ;
         this.roundPiles.add( aRoundPile ) ;
+        // If this is the first RoundPile:
+        if ( this.roundPiles.size() == 1 )
+            {
+            this.highestCard = aRoundPile.getHighestCard() ;
+            this.lowestCard = aRoundPile.getLowestCard() ;
+            this.highestRoundPile = aRoundPile ;
+            this.lowestRoundPile = aRoundPile ;
+            return ;
+
+            }
+
+        // If this isn't the first RoundPile, compare RoundPile's cards
+        if ( this.highestCard.compareTo( aRoundPile.getHighestCard() ) < 0 )
+            {
+            // Sets the new highest card and owner if newCard is higher
+            this.highestCard = aRoundPile.getHighestCard() ;
+            this.highestRoundPile = aRoundPile ;
+
+            }
+
+        if ( this.lowestCard.compareTo( aRoundPile.getLowestCard() ) > 0 )
+            {
+            // Sets the new lowest card and owner if newCard is lower
+            this.lowestCard = aRoundPile.getLowestCard() ;
+            this.lowestRoundPile = aRoundPile ;
+
+            }
 
         }   // end addRoundPile()
 
@@ -70,9 +104,70 @@ public class Team
     public void addScore( int value )
         {
         // DONE implement this
-        this.score = this.score + value ;
+        this.score += value ;
 
         }   // end addScore()
+
+
+    /**
+     * @param deck
+     *     deals each card in each RoundPile back to a deck
+     */
+    public void dealBack( Deck deck )
+        {
+        for ( RoundPile roundPile : this.roundPiles )
+            {
+            for ( int i = 0 ; i < 4 ; i++ )
+                {
+                deck.add( roundPile.remove() ) ;
+
+                }
+
+            }
+
+        }   // end dealBack()
+
+
+    /**
+     * @return the highest card out of all of the RoundPiles in the current team
+     */
+    public Card getHighestCard()
+        {
+        return this.highestCard ;
+
+        }   // end HighestCard()
+
+
+    /**
+     * @return the RoundPile that's responsible for the highest card in the current
+     *     Team
+     */
+    public RoundPile getHighestRoundPile()
+        {
+        return this.highestRoundPile ;
+
+        }   // end HighestRoundPile()
+
+
+    /**
+     * @return the lowest card out of all of the RoundPiles in the current team
+     */
+    public Card getLowestCard()
+        {
+        return this.lowestCard ;
+
+        }   // end LowestCard()
+
+
+    /**
+     * @return the RoundPile that's responsible for the lowest card in the current
+     *     Team
+     */
+    public RoundPile getLowestRoundPile()
+        {
+        return this.lowestRoundPile ;
+
+        }   // end lowestRoundPile()
 
 
     /**
@@ -97,8 +192,16 @@ public class Team
         }   // end getScore()
 
 
+    /** @return tallyPoints of current team */
+    public int getTallyPoints()
+        {
+        return this.tallyPoints ;
+
+        }
+
+
     /**
-     * refreshes score to 0
+     * refreshes tallyPoints to 0
      */
     public void refresh()
         {
@@ -106,28 +209,9 @@ public class Team
          * Refreshes every variable except players
          */
         // DONE implement this
-        this.score = 0 ;
+        this.tallyPoints = 0 ;
 
         }   // end refresh()
-
-
-    /**
-     * @param deck
-     *     deals each card in each RoundPile back to a deck
-     */
-    public void dealBack( Deck deck )
-        {
-        for ( RoundPile roundPile : this.roundPiles )
-            {
-            for ( int i = 0 ; i < 4 ; i++ )
-                {
-                deck.add( roundPile.remove() ) ;
-
-                }
-
-            }
-
-        }   // end dealBack()
 
     }
 // end class Team
